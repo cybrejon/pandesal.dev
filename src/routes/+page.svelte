b50786246be58c06f0d6e1b3b0d552ac4fbb48b0
cybrejon <cybrejon.senpai@gmail.com>
2025-11-08 00:10:52 +0800

    chore: new logo and miscellaneous changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,5 @@
-<div class="h-svh p-2 sm:p-6">
-	<div class="relative h-full overflow-hidden rounded-3xl border border-foreground/15">
-		<div class="absolute top-1/2 left-1/2 h-1 w-1">
-			<div class="h-50 w-50 -translate-x-90 -translate-y-48 rounded-full bg-primary/50"></div>
-		</div>
-		<div class="h-full bg-card/20 p-7 backdrop-blur-[140px]">
-			<div class="relative flex items-center justify-center pb-4">
-				<div class="absolute left-0 flex items-center gap-4">
-					{#each ['bg-ctp-red-200', 'bg-accent', 'bg-ctp-blue-200'] as color (color)}
-						<div class={`h-5 w-5 rounded-full ${color}`}></div>
-					{/each}
-				</div>
-				<p class="text-xl tracking-wider opacity-60">./pandev</p>
-			</div>
-			<div class="flex h-full flex-col items-center justify-center">
-				<img class="w-50 md:w-auto" src="/images/logo.png" alt="Pandev Community Logo - A Community of Filipino Software Developers" />
-				<div class="flex flex-col items-center gap-6 text-center md:gap-12">
-					<h1
-						class="max-w-[60rem] font-dela text-3xl font-light tracking-wide text-pretty drop-shadow-[0_12px_var(--color-background)] md:text-5xl lg:text-7xl lg:leading-20"
-					>
-						A Comm<span class="text-secondary">unity</span> of
-						<span class="text-accent">Filipino</span> Software Developers
-					</h1>
-					<p class="text-base drop-shadow-[0_12px_var(--color-background)] md:text-2xl">
-						Pandev is where the fun tech talks take place
-					</p>
-					<a
-						class="flex items-center gap-3 rounded-full border-2 border-ctp-mauve-400 bg-linear-to-b from-ctp-mauve-800 via-ctp-mauve-800 to-ctp-mauve-950 px-4 py-2 drop-shadow-[0_12px_var(--color-background)] transition-all hover:-translate-y-1 active:translate-y-2 active:opacity-60 active:drop-shadow-none md:px-7 md:py-4"
-						href="https://discord.gg/pandesal-dev"
-					>
-						<span class="iconify text-2xl text-white mingcute--discord-fill md:text-[2.5rem]"
-						></span>
-						<span class="text-xl font-bold text-white md:text-2xl">Join Discord</span></a
-					>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+<script>
+	import Hero from '$components/sections/Hero.svelte';
+</script>
+
+<Hero />
