df5b32aef1fbc3cb23a13d19afbf8674807bb543
cybrejon <cybrejon.senpai@gmail.com>
2026-05-02 21:21:36 +0800

    chore: improve Container component

--- src/components/atoms/Container.svelte
+++ src/components/atoms/Container.svelte
@@ -3,13 +3,14 @@
 	interface Props {
 		children: Snippet;
 		class?: string;
+		classInner?: string;
 	}
-	let { children, class: className }: Props = $props();
+	let { children, class: className, classInner }: Props = $props();
 </script>
 
-<div class="px-2 pb-2 sm:px-6 sm:pb-6">
+<div class="px-2 pb-2 sm:px-6 sm:pb-6 {className}">
 	<div
-		class="relative h-full overflow-hidden rounded-3xl border border-foreground/15 bg-card/20 p-4 md:p-7 {className}"
+		class="relative h-full overflow-hidden rounded-3xl border border-foreground/15 bg-card/20 p-4 md:p-12 {classInner}"
 	>
 		{@render children()}
 	</div>
